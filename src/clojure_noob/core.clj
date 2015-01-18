af73e1f6a2607eef0cf1b48c58ba197b5a21353e
Charles Griffin <cgriffin@indatus.com>
2015-01-18 06:41:25 -0500

    solved find-the-sum-of-all-the-multiples-of-3-or-5-below-1000

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -1,15 +1,17 @@
 (ns clojure-noob.core
   (:gen-class))
 
+;;If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+;;Find the sum of all the multiples of 3 or 5 below 1000.
+
+(defn multiples-of-3-or-5
+  [number-in-range]
+  (if (or (= 0 (mod number-in-range 3)) (= 0 (mod number-in-range 5)))
+    true
+    false))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (def severity :mild)
-  (def error-message "OH GOD! II'S A DISASTER! WE'RE ")
-  (if (= severity :mild)
-      (def error-message (str error-message "MIILDLY INCONVENIENCED!"))
-      (def error-message (str error-message "DOOOMED!!"))
 
-    )
-
-  )
+  (println (reduce + (filter multiples-of-3-or-5 (range 0 1000)))))
