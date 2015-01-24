aec0ad6c1e2734135a0d884f50f7b2c6dfefdfbd
Charles Griffin <cgriffin@indatus.com>
2015-01-23 22:38:49 -0500

    finished and understand the fwpd example

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -3,9 +3,12 @@
   ;; We haven't gone over require but we will.
   (:require [clojure.string :as s]))
 
+(defmacro dbg[x] `(let [x# ~x] (println "dbg:" '~x "=" x#) x#))
+
 (def filename "suspects.csv")
 
-(def headers->keywords {"Name" :name "Glitter Index" :glitter-index})
+(def headers->keywords {"Name" :name
+                        "Glitter Index" :glitter-index})
 
 (defn str->int
   [str]
@@ -15,14 +18,38 @@
   "convert a csv into rows of columns"
   [string]
   (map #(s/split % #",")
-       #(s/split string #" ")))
+       (s/split string #"\n")))
+
 
 ;; CSV is all text, but we're storing numeric data. We want to convert
 ;; it back to actual numbers.
 (def conversions {:name identity
                   :glitter-index str->int})
 
+(defn mapify
+  "Return a seq of maps like {:name \"Edward Cullen\" :glitter-index 10}"
+  [rows]
+  (let [;; headers becomes the seq (:name :glitter-index)
+        headers (map #(get headers->keywords %) (first rows))
+        ;; unmapped-rows becomes the seq
+        ;; (["Edward Cullen" "10"] ["Bella Swan" "0"] ...)
+        unmapped-rows (rest rows)]
+
+    ;; Now let's return a seq of {:name "X" :glitter-index 10}
+    (map (fn [unmapped-row]
+              ;; We're going to use map to associated each header with its
+              ;; column.  Since map returns a seq, we use "into" to convert
+              ;; it into a map
+              (into {}
+                    ;; notice we're passing multiple collections to map
+                    (map (fn [header column]
+                           ;; associate the header with the converted column
+                           [header ((get conversions header) column)])
+                         headers
+                         unmapped-row)))
+     unmapped-rows)))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println (slurp filename)))
+  (println (mapify (parse (slurp filename)))))
