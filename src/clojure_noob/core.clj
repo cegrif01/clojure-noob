0dd6664e30723eed6503d2c81175711212d36848
Charles Griffin <cgriffin@indatus.com>
2015-04-13 22:52:00 -0400

    compound interest works... yay

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -3,93 +3,28 @@
   ;; We haven't gone over require but we will.
   (:require [clojure.string :as s]))
 
-(defmacro dbg[x] `(let [x# ~x] (println "dbg:" '~x "=" x#) x#))
 
-(def filename "suspects.csv")
+(defn simple-interest "calculate simple interest and total amount on a principle for a given rate and time"
+    [principle rate period]
+    (let [si (/ (* principle rate period) 100.0)
+          amount (+ principle si)]
+      {:interest si :amount amount}));
 
-(def headers->keywords {"Name" :name
-                        "Glitter Index" :glitter-index})
-
-(defn fetch-by-key
-  [recs key]
-  (map (fn[rec]
-         (get rec key))
-   recs))
-
-(defn str->int
-  [str]
-  (Integer. str))
-
-(defn parse
-  "convert a csv into rows of columns"
-  [string]
-  (map #(s/split % #",")
-       (s/split string #"\n")))
-
-(defn validate?
-  [func-map rec-to-validate]
-  (let [status (map
-                  (fn [func-key]((get func-map func-key) rec-to-validate))
-                (keys func-map))]
-
-               (reduce #(and %1 %2) status)))
-
-(def validation-map {:name #(contains? % :name) :glitter-index #(contains? % :glitter-index)})
-
-(defn prepend
-  [recs rec-to-add]
-  (if (validate? validation-map rec-to-add)
-      (conj (into () recs) rec-to-add)
-      (println "invalid record")))
+(defn compound-interest "calculate compound intest and total amount.  Assuming compounding per year"
+    [principle nominal-interest-rate times-compounded period]
+    (let [total-amount (* principle (Math/pow (+ 1 (/ (/ nominal-interest-rate 100) times-compounded)) (* times-compounded period)))
+          interest (- total-amount principle)]
+    {:total total-amount :interest interest}));
 
 
-;; CSV is all text, but we're storing numeric data. We want to convert
-;; it back to actual numbers.
-(def conversions {:name identity
-                  :glitter-index str->int})
-
-(defn glitter-filter
-  [minimum-glitter records]
-  (filter #(>= (:glitter-index %) minimum-glitter) records))
-
-
-(defn mapify
-  "Return a seq of maps like {:name \"Edward Cullen\" :glitter-index 10}"
-  [rows]
-  (let [;; headers becomes the seq (:name :glitter-index)
-        headers (map #(get headers->keywords %) (first rows))
-        ;; unmapped-rows becomes the seq
-        ;; (["Edward Cullen" "10"] ["Bella Swan" "0"] ...)
-        unmapped-rows (rest rows)]
-
-    ;; Now let's return a seq of {:name "X" :glitter-index 10}
-    (map (fn [unmapped-row]
-              ;; We're going to use map to associated each header with its
-              ;; column.  Since map returns a seq, we use "into" to convert
-              ;; it into a map
-              (into {}
-                    ;; notice we're passing multiple collections to map
-                    (map (fn [header column]
-                           ;; associate the header with the converted column
-                           [header ((get conversions header) column)])
-                         headers
-                         unmapped-row)))
-     unmapped-rows)))
-
-(defn to-csv
-  [rows]
-    (println rows)
-  )
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  ;;(println (fetch-by-key (glitter-filter 3 (mapify (parse (slurp filename)))) :name))
-  ;;(println (fetch-by-key [{:name "Charles" :glitter-index 5} {:name "Mandy" :glitter-index 10}] :name))
-  ;;(println ((get {:name #(contains? % :name) :glitter-index #(contains? % :glitter-index)} :glitter-index) {:nam "Gilbert" :glitter-inex 12}) )
 
-  ;;(println (validate? {:name #(contains? % :name) :glitter-index #(contains? % :glitter-index)} {:name "Gilbert" :glitter-index "12"}))
-  ;;(println (prepend [{:name "Gilbert" :glitter-inex 12} {:name "Stuff" :glitter-index 2}] {:name "Ooeey" :glitter-index 4}))
-  (println (to-csv [{:name "Gilbert" :glitter-index 12} {:name "Stuff" :glitter-index 2}]))
+  ;;(println (simple-interest 10000 4 5))
+  (println (compound-interest 1500 4.3 4 6))
+
+
 
   )
