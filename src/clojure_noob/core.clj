13d588f7cb108c2387338441b90bd456b987a8f0
Charles Griffin <cgriffin@indatus.com>
2015-04-14 21:45:27 -0400

    figured out how to get the current time stamp in clojure

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -4,6 +4,12 @@
   (:require [clojure.string :as s]))
 
 
+(defn parse
+  "Convert a csv into rows of columns"
+  [string]
+  (map #(s/split % #",")
+       (s/split string #"\n")))
+
 (defn simple-interest "calculate simple interest and total amount on a principle for a given rate and time"
     [principle rate period]
     (let [si (/ (* principle rate period) 100.0)
@@ -11,19 +17,25 @@
       {:interest si :amount amount}));
 
 (defn compound-interest "calculate compound intest and total amount.  Assuming compounding per year"
-    [principle nominal-interest-rate times-compounded period]
-    (let [total-amount (* principle (Math/pow (+ 1 (/ (/ nominal-interest-rate 100) times-compounded)) (* times-compounded period)))
+    [principle nominal-interest-rate times-compounded-per-year period]
+    (let [total-amount (* principle (Math/pow (+ 1 (/ (/ nominal-interest-rate 100) times-compounded-per-year)) (* times-compounded-per-year period)))
           interest (- total-amount principle)]
-    {:total total-amount :interest interest}));
+    {:total total-amount :interest interest :principle principle :loan-length period}));
 
+
+(defn make-payment-schedule "From the given payment information, starting date of loan, loan length, it will generate a monthly payment schedule"
+    [principle interest-rate times-compounded-per-year loan-length starting-date]
+    (let [payment-info (compound-interest principle interest-rate times-compounded-per-year starting-date)]
+          starting-date))
 
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
 
-  ;;(println (simple-interest 10000 4 5))
-  (println (compound-interest 1500 4.3 4 6))
+;;  (println (compound-interest 10769.93 5 12 5))
+  (println (make-payment-schedule 10769.93 5 12 5 (quot (System/currentTimeMillis) 1000)))
+
 
 
 
