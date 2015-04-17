3bf8bfa7d984141c03956ad35bc7b0b137c34e07
Charles Griffin <cgriffin@indatus.com>
2015-04-17 18:07:46 -0400

    amorization table works... yayyy

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -1,7 +1,8 @@
 ;; In ns below, notice that "gen-class" was removed
 (ns clojure-noob.core
   ;; We haven't gone over require but we will.
-  (:require [clojure.string :as s]))
+  (:require [clojure.string :as s])
+  (:require [clojure.pprint :as p]))
 
 (defn now
       "Returns the current epoch time"
@@ -36,9 +37,6 @@
           number-of-payments     (* loan-length times-compounded-per-year)]
       number-of-payments));
 
-(defn create-amorization-schedule
-      [] )
-
 (defn amorization-interest-rate
   [interest-rate number-of-times-per-year]
   (/ (/ interest-rate number-of-times-per-year) 100))
@@ -51,31 +49,31 @@
 
 (defn interest-principle-breakdown "Determines how much of a monthly payment goes towards interest and towards the actual loan amount"
   [monthly-payment interest-rate loan-balance]
-  (let [interest          (* loan-balance interest-rate)
-        principle         (- monthly-payment interest)
-        remaining-amount  (- loan-balance principle)]
+  (let [interest          (float (* loan-balance interest-rate))
+        principle         (float (- monthly-payment interest))
+        remaining-amount  (float (- loan-balance principle))]
     {:amount-towards-interest interest, :amount-towards-principle principle, :remaining-amount remaining-amount}
 ));
+
+(defn create-amorization-schedule "returns a map of the break down of each payment. This breakdown will include the amount remaining on principle and how much of your payment went towards interest"
+    [loan-amount monthly-payment interest]
+    (loop [amount   loan-amount
+           schedule []]
+      (if (< amount 0)
+        schedule
+        (let [breakdown     (interest-principle-breakdown monthly-payment interest amount)
+              new-amount    (get breakdown :remaining-amount)
+              new-schedule  (conj schedule breakdown)]
+          (recur new-amount new-schedule)))));
+
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
 
-;;  (println (compound-interest 10769.93 5 12 5))
-;;  (println (make-payment-schedule 10769.93 5 12 5 (quot (System/currentTimeMillis) 1000)))
-
-;;  (println (make-payment-schedule 10769.93 5 12 5))
-
-;;    (println (simple-interest 10769.93 5 5))
-
-;;    (println (/ 2692.48 60)) ;;44.87
-;;    (println (/ 10769.93 60)) ;;179.50
-
-;;    (println (+ (/ 13462.41 60)))
-;;    (println (/ (* 10769.93 (+ 1 (* (/ 5 100) 5))) 60) )
-
   (def interest (amorization-interest-rate 5 12))
-  (def monthly-payment (monthly-amorization-payment-amount 10769.93 (amorization-interest-rate 5 12) 60))
-  (println (interest-principle-breakdown monthly-payment interest 10769.93))
+  (def monthly-payment (monthly-amorization-payment-amount 10769.93 interest 60))
+  ;;(println (interest-principle-breakdown monthly-payment interest 10769.93))
+  (p/print-table (create-amorization-schedule 10769.93 monthly-payment interest))
 
 )
