71dd0986e22b6c91c123587513946271a2690910
Charles Griffin <cgriffin@indatus.com>
2015-05-10 13:51:25 -0400

    I now have the solution.  I just have to get in the format that code eval expects

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -1,45 +1,25 @@
-;; In ns below, notice that "gen-class" was removed
-(ns clojure-noob.core
-  (:require [clojure.pprint :as p])
-  (:require [clojure.string :as str]))
+(ns clojure-noob.core)
 
   (defn split-by-space
     [x]
-    (str/split x #"\s"))
+    (clojure.string/split x #"\s"))
 
-  (defn split-up-words
-    [line]
-    (str/split line #"\n"))
+  (defn to-sentence
+    "transforms a collection of strings to a sentence"
+    [coll]
+    (clojure.string/join " " coll))
 
-;;     (def s "Hello World\nHello CodeEval")
-;;   (def split-up-words (clojure.string/split s #"\n"))
+  (defn -main
+    ""
+    [& args]
 
-;; split-up-words
+     (println "Place type in name of file")
+     (def file (read-line))
 
-
-
-;; (split-by-space split-up-words)
-
-
-(defn -main
-  "Takes in loan information and creates an amorization schedule"
-  [& args]
-
-   (println "Place type in name of file")
-   (def file (read-line))
-
-  ;;   ; Sample code to read in test cases:
-  ;;   ; Open the file passed as the first command line argument
-  (with-open [rdr (clojure.java.io/reader file)]
-  ; Read each line ignoring empty ones
-  (doseq [line (remove empty? (line-seq rdr))]
-    ;;(println (vector line))))
-    (println (first (split-by-space line)))))
-
-
-;;     (def lines '(["Hot" "Damn" "Cheese"] ["Hello" "CodeEval"]))
-;;     (println lines)
-;;    (println (map #(sort-by last %) lines))
-
-;; (apply str (map #(str (s/join ", " %) "\n") data-values))
-)
+    ;;Sample code to read in test cases:
+    ;;Open the file passed as the first command line argument
+    ;;(with-open [rdr (clojure.java.io/reader (first *command-line-args*))]
+    (with-open [rdr (clojure.java.io/reader file)]
+    ; Read each line ignoring empty ones
+    (doseq [line (remove empty? (line-seq rdr))]
+      (println (to-sentence (rseq (split-by-space line)))))))
